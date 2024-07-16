0c2f47d621bbdfa9e80b2a8f927f3fa02343f56e
Prakash <dgsarve@gmail.com>
2024-07-16 13:26:24 +0200

    history will be saved in async remove login from router build in fe

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,14 +1,13 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Page from "@/app/page";
-import Login from "@/app/login/page";
+
 
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
-                <Route path="/login" element={<Login />} />
                 <Route path="/" element={<Page />} />
             </Routes>
         </Router>
